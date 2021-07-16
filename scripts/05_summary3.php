ce972a1505561130010a230746dcf765b9e0daa9
kiang <kiange@gmail.com>
2021-07-16 15:59:30 +0800

    generate details of summary3

--- scripts/05_summary3.php
+++ scripts/05_summary3.php
@@ -1,18 +1,19 @@
 <?php
 $basePath = dirname(__DIR__);
+$cityPath = $basePath . '/data/summary3';
 
-$count = [
-    'keep' => 0,
-    'plus' => 0,
-    'minus' => 0,
-];
+$fh = [];
 foreach (glob($basePath . '/data/slip/*/*.json') as $jsonFile) {
     $jsonFile109 = str_replace('/slip/', '/slip109/', $jsonFile);
     if(file_exists($jsonFile109)) {
         $json = json_decode(file_get_contents($jsonFile), true);
         $json109 = json_decode(file_get_contents($jsonFile109), true);
+        $city = $json['meta']['city'];
         $pool = [];
-        $sumDiff = 0;
+        if (!isset($fh[$city])) {
+            $fh[$city] = fopen($cityPath . '/' . $city . '.csv', 'w');
+            fputcsv($fh[$city], ['鄉鎮市區', '學校', '類型', '年齡', '學期', '班別', '109全學期總收費', '110全學期總收費']);
+        }
 
         foreach ($json['slip'] as $y => $l1) {
             if (empty($y)) {
@@ -42,31 +43,21 @@ foreach (glob($basePath . '/data/slip/*/*.json') as $jsonFile) {
             foreach ($l1 as $p => $l2) {
                 foreach ($l2['class'] as $class => $l3) {
                     if (!empty($l3['全學期總收費']['小計'])) {
-                        if(!isset($pool[$y])) {
-                            $pool[$y] = [];
-                        }
-                        if(!isset($pool[$y][$p])) {
-                            $pool[$y][$p] = [];
-                        }
-                        if(!isset($pool[$y][$p][$class])) {
-                            $pool[$y][$p][$class] = [];
-                        }
-                        $pool[$y][$p][$class][109] = $l3['全學期總收費']['小計'];
-                        if(isset($pool[$y][$p][$class][110])) {
-                            $sumDiff += $pool[$y][$p][$class][110] - $pool[$y][$p][$class][109];
+                        if(isset($pool[$y][$p][$class][110]) && $pool[$y][$p][$class][110] != $l3['全學期總收費']['小計']) {
+                            fputcsv($fh[$city], [
+                                $json['meta']['town'],
+                                $json['meta']['title'],
+                                $json['meta']['type'],
+                                $y,
+                                $p,
+                                $class,
+                                $l3['全學期總收費']['小計'],
+                                $pool[$y][$p][$class][110],
+                            ]);
                         }
                     }
                 }
             }
         }
-        if($sumDiff == 0) {
-            $count['keep'] += 1;
-        } elseif($sumDiff > 0) {
-            $count['plus'] += 1;
-        } else {
-            $count['minus'] += 1;
-        }
     }
 }
-
-print_r($count);
