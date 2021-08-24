d171f77c7198a9cf208dfeff2343f27f6e4af1b9
kiang <kiange@gmail.com>
2021-08-24 23:08:05 +0800

    slip.php for batch processing after game/fast.php

---
+++ scripts/slip.php
@@ -0,0 +1,16 @@
+<?php
+$rootPath = dirname(__DIR__);
+
+$now = date('Y-m-d H:i:s');
+
+exec("cd {$rootPath} && /usr/bin/git pull");
+
+exec("php -q {$rootPath}/scripts/04_slip2data.php"); // convert slip html to data
+exec("php -q {$rootPath}/scripts/05_summary1.php"); // calculate fee summary for using in icon label
+exec("php -q {$rootPath}/scripts/03_geocoding.php"); // generate final geojson
+
+exec("cd {$rootPath} && /usr/bin/git add -A");
+
+exec("cd {$rootPath} && /usr/bin/git commit --author 'auto commit <noreply@localhost>' -m 'auto update @ {$now}'");
+
+exec("cd {$rootPath} && /usr/bin/git push origin master");
