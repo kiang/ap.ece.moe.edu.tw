c5c0290f3fa74540f2dd8707695988ac399a3aa7
kiang <kiange@gmail.com>
2024-02-20 22:44:14 +0800

    add script to generate kids_vehicles.json

---
+++ scripts/01_kids_vehicle.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * open data from https://data.gov.tw/dataset/96649
+ */
+$basePath = dirname(__DIR__);
+include $basePath . '/vendor/autoload.php';
+
+use Goutte\Client;
+
+$client = new Client();
+$client->request('GET', 'https://www.thb.gov.tw/Common/ThbOpenDataService.ashx?SN=589&format=1');
+$csvFile = $basePath . '/raw/kids_vehicles.csv';
+file_put_contents($csvFile, $client->getResponse()->getContent());
+
+$areas = json_decode(file_get_contents(__DIR__ . '/areas.json'), true);
+$codes = $pool = [];
+foreach ($areas as $city => $data1) {
+    foreach ($data1 as $town => $code) {
+        $codes[$city . $town] = $code;
+    }
+}
+
+$fc = json_decode(file_get_contents($basePath . '/docs/preschools.json'), true);
+foreach ($fc['features'] as $f) {
+    if (isset($codes[$f['properties']['city'] . $f['properties']['town']])) {
+        $code = $codes[$f['properties']['city'] . $f['properties']['town']];
+    } else {
+        $parts = preg_split('/[^0-9]+/', $f['properties']['address']);
+        $code = $parts[1];
+    }
+
+    if (!isset($pool[$code])) {
+        $pool[$code] = [];
+    }
+    $pool[$code][$f['properties']['title']] = $f['properties']['id'];
+}
+
+$fh = fopen($csvFile, 'r');
+$head = fgetcsv($fh, 2048);
+$head[0] = 'plate_no';
+$result = [];
+while ($line = fgetcsv($fh, 2048)) {
+    $data = array_combine($head, $line);
+    if (isset($pool[$data['owner_zip']][$data['owner_name']])) {
+        $id = $pool[$data['owner_zip']][$data['owner_name']];
+        if (!isset($result[$id])) {
+            $result[$id] = [];
+        }
+        unset($data['owner_zip']);
+        unset($data['owner_name']);
+        $result[$id][] = $data;
+    }
+}
+
+file_put_contents($basePath . '/docs/kids_vehicles.json', json_encode($result, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
--- scripts/slip.php
+++ scripts/slip.php
@@ -8,6 +8,7 @@ exec("cd {$rootPath} && /usr/bin/git pull");
 exec("php -q {$rootPath}/scripts/04_slip2data.php"); // convert slip html to data
 exec("php -q {$rootPath}/scripts/05_summary1.php"); // calculate fee summary for using in icon label
 exec("php -q {$rootPath}/scripts/03_geocoding.php"); // generate final geojson
+exec("php -q {$rootPath}/scripts/01_kids_vehicle.php"); // generate final geojson
 
 exec("cd {$rootPath} && /usr/bin/git add -A");
 
