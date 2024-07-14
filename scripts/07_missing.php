84c9e4d22800f5e055fefaa70015a3ade3b9885a
kiang <kiange@gmail.com>
2024-07-14 21:27:55 +0800

    fix id mis-matching issue

--- scripts/07_missing.php
+++ scripts/07_missing.php
@@ -20,6 +20,7 @@ foreach (glob($basePath . '/git_preschools/*.json') as $jsonFile) {
         $data = $f['properties'];
         $key = $data['reg_docno'] . $data['title'];
         if (isset($idPool[$key])) {
+            $f['properties']['id'] = $idPool[$key];
             $targetFile = $featurePath . '/' . $idPool[$key] . '.json';
             file_put_contents($targetFile, json_encode($f, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
         }
