ad33a45e0f8c1d598cb7cd2864fd4a0736f39ca7
kiang <kiange@gmail.com>
2024-07-14 13:32:41 +0800

    remove duplicated id

--- scripts/fix.php
+++ scripts/fix.php
@@ -9,19 +9,7 @@ if (file_exists($idFile)) {
     }
 }
 
-$oFh = fopen($basePath . '/docs/data/id/idn.csv', 'w');
-foreach (glob($basePath . '/docs/data/*.csv') as $csvFile) {
-    $fh = fopen($csvFile, 'r');
-    $head = fgetcsv($fh, 2048);
-    while ($line = fgetcsv($fh, 2048)) {
-        $data = array_combine($head, $line);
-        if (false !== strpos($data['reg_no'], '負責人')) {
-            continue;
-        }
-        $key = $data['reg_no'] . $data['title'];
-        $newKey = $data['reg_docno'] . $data['title'];
-        if (isset($idPool[$key])) {
-            fputcsv($oFh, [$newKey, $idPool[$key]]);
-        }
-    }
+$oFh = fopen($basePath . '/docs/data/id/id.csv', 'w');
+foreach ($idPool as $k => $v) {
+    fputcsv($oFh, [$k, $v]);
 }
