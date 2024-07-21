ac334b279904af42e0b71731abb270893f3a157c
kiang <kiange@gmail.com>
2024-07-21 20:50:10 +0800

    update to use 113 paths in slip fetching

--- game/fast.php
+++ game/fast.php
@@ -33,7 +33,7 @@ foreach (glob($basePath . '/docs/data/*.csv') as $csvFile) {
         if ($data['has_slip'] === 'no') {
             continue;
         }
-        $rawPath = $basePath . '/raw/slip112/' . $data['city'];
+        $rawPath = $basePath . '/raw/slip113/' . $data['city'];
         if (!file_exists($rawPath)) {
             mkdir($rawPath, 0777, true);
         }
