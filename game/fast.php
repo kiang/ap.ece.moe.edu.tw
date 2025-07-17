3625b57cbe3fba0c6cdcc923fca5373ae500d66a
kiang <kiange@gmail.com>
2025-07-17 10:54:16 +0800

    add more parameters for tesseract

--- game/fast.php
+++ game/fast.php
@@ -92,7 +92,7 @@ foreach (glob($basePath . '/docs/data/*.csv') as $csvFile) {
                      * 
                      * tessedit_char_whitelist abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890
                      */
-                    exec('/usr/bin/tesseract ' . __DIR__ . '/qq.png ' . __DIR__ . '/qq letters');
+                    exec('/usr/bin/tesseract ' . __DIR__ . '/qq.png ' . __DIR__ . '/qq --psm 8 -c load_system_dawg=0 -c load_freq_dawg=0 letters');
                     $ans = file_get_contents(__DIR__ . '/qq.txt');
                     $ans = preg_replace('/[^0-9a-z]+/i', '', $ans);
                     if (strlen($ans) === 5) {
